af9f2bf5e0db0c6c562689050eb04d0958d48aba
Pascal LUCAS <89383863+Pascal1494@users.noreply.github.com>
2022-10-26 19:06:55 +0200

    Update WorkCardData.jsx

--- src/Components/WorkCardData.jsx
+++ src/Components/WorkCardData.jsx
@@ -7,21 +7,21 @@ const ProjectCardData = [
 imgsrc: pro1,
 title: "Portfolio react-responsive",
 text: "Création d'un portfolio personnel afin de montrer une partiee des mes compétences",
-view: "https://github.com/Pascal1494/porfolio-react",
+view: "View",
 
 },
 {
 imgsrc: pro2,
 title: "Drum-react-box",
 text: "Création d'une boite à rythmes. Ce projet a été réalisé pendant le cous sur le Framework 'React' ",
-view: "#",
+view: "View",
 
 },
 {
 imgsrc: pro3,
 title: "le Dé de 100",
 text: "Création d'un jeu ou le joueur lance le dé, tant qu'il ne fait pas 1 il peut continuer à additiooner les scores de son dé. Libre à lui de consigner le score avant de faire un. Sinon ce sera à l'autre joueur de tenter sa chance. Le premier qui réalise 100 points gagne la partie", 
-view: "#",
+view: "View",
 
 },
 ];
